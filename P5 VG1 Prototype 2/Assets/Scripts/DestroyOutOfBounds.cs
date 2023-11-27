1794705b62697566a8084ff3196d0819b2a83b6b
kimlongvo <hello@kimlongvo.com>
2023-11-27 13:09:19 -0800

    prefab animals + destroy prefabs out of bounds

---
+++ P5 VG1 Prototype 2/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30f;
+    private float lowerBound = -10f;
+
+    void Update()
+    {
+        if(transform.position.z > topBound || transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
