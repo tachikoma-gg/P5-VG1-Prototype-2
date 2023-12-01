f96cde79eb8f541a9766e2a41b5d3c9df75053ac
kimlongvo <hello@kimlongvo.com>
2023-12-01 14:00:47 -0800

    adjust spawnmanager

--- P5 VG1 Prototype 2/Assets/Scripts/SpawnManager.cs
+++ P5 VG1 Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] animalPrefabs;
-    [SerializeField] private int animalIndex;
+    [SerializeField] private float spawnRangeX = 20f;
+    [SerializeField] private float spawnPositionZ = 20f;
 
     void Start()
     {
@@ -14,9 +15,17 @@ public class SpawnManager : MonoBehaviour
 
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.P))
+        if(Input.GetKey(KeyCode.P))
         {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+            float x = Random.Range(-spawnRangeX, spawnRangeX);
+            float y = 0;
+            float z = spawnPositionZ;
+
+            Vector3 spawnPosition = new Vector3(x, y, z);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
