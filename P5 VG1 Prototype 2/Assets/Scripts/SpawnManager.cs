ffb1393e28ebcb0044a131971820b8338b27ddeb
kimlongvo <hello@kimlongvo.com>
2023-12-04 13:42:01 -0800

    made animals spawn automatically
    used invoke repeating
    created new SpawnRandomAnimal method.

--- P5 VG1 Prototype 2/Assets/Scripts/SpawnManager.cs
+++ P5 VG1 Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -7,25 +7,24 @@ public class SpawnManager : MonoBehaviour
     [SerializeField] private GameObject[] animalPrefabs;
     [SerializeField] private float spawnRangeX = 20f;
     [SerializeField] private float spawnPositionZ = 20f;
+    [SerializeField] private float startDelay = 3f;
+    [SerializeField] private float spawnInterval = 0.75f;
 
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
-    void Update()
+    private void SpawnRandomAnimal()
     {
-        if(Input.GetKey(KeyCode.P))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
 
-            float x = Random.Range(-spawnRangeX, spawnRangeX);
-            float y = 0;
-            float z = spawnPositionZ;
+        float x = Random.Range(-spawnRangeX, spawnRangeX);
+        float y = 0;
+        float z = spawnPositionZ;
 
-            Vector3 spawnPosition = new Vector3(x, y, z);
+        Vector3 spawnPosition = new Vector3(x, y, z);
 
-            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
-        }
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
     }
 }
